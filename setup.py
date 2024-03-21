cbc5d39de138a951a239814b79a08c67565c4b8e
bryanptom <bryanptom@gmail.com>
2024-03-21 12:54:31 -0400

    setup initial

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="news_deja_vu",  # Required
+    version="0.0.1",  # Required
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.7, <4",
+)
